20c6206785192d89bd5dccf2baed619cddba4b89
Lizzie Kim <jihyunca@gmail.com>
2020-06-07 16:20:51 -0400

    Updates

---
+++ precache-manifest.1096b302671953059b831fa64a737527.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc8e0ef8ec79c46f4de4120a317cc3bf",
+    "url": "/static/media/hotel-paranoia.fc8e0ef8.png"
+  },
+  {
+    "revision": "ec6a94f758e9d4708f90",
+    "url": "/static/css/main.bf87e0cd.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8bb5bbc73e4c316214d3",
+    "url": "/static/js/2.59b51bf6.chunk.js"
+  },
+  {
+    "revision": "64a8dbe1fff50095d9972b9859b1c25f",
+    "url": "/static/media/hack.64a8dbe1.png"
+  },
+  {
+    "revision": "ec6a94f758e9d4708f90",
+    "url": "/static/js/main.db1c9bf8.chunk.js"
+  },
+  {
+    "revision": "02cd7b94c3daab3420c1a8ff8dfc9f5f",
+    "url": "/static/media/ffc.02cd7b94.png"
+  },
+  {
+    "revision": "090539eaa97f5e9a198cff41bbd4f6d1",
+    "url": "/static/media/waterloo.090539ea.png"
+  },
+  {
+    "revision": "28dd03f6c7ea2b0649b1053db881d5b1",
+    "url": "/static/media/mapleleaf.28dd03f6.png"
+  },
+  {
+    "revision": "2bfe3ee2145f6755e4b5960310daee03",
+    "url": "/static/media/Tajawal-Medium.2bfe3ee2.ttf"
+  },
+  {
+    "revision": "e0c853f2436692c6c6ba90ba1cd5038d",
+    "url": "/index.html"
+  }
+];
